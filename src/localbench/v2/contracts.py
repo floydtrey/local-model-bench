ef1949873e2141aee903f0ca6eb292e4d837f691
floydtrey <floydtrey@ymail.com>
2026-09-11 23:54:22 -0500

    Add execution interface evidence record type

--- src/localbench/v2/contracts.py
+++ src/localbench/v2/contracts.py
@@ -20,6 +20,7 @@ RECORD_TYPES = frozenset(
         "benchmark_input",
         "evaluator_identity",
         "trial_identity",
+        "execution_interface_identity",
         "execution_binding",
         "run_manifest",
         "case_result",
